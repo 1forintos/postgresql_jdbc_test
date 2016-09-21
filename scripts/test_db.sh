7a76034f4c7dacd2d064f4540b90a737985da4ae
1forintos <xrensis@gmail.com>
2016-09-21 16:53:37 +0200

    Added new notes. ALtered db testing script.

--- scripts/test_db.sh
+++ scripts/test_db.sh
@@ -3,13 +3,13 @@ while true
 do
 	echo "TEST BEGIN"
 	free && sync && echo 3 > /proc/sys/vm/drop_caches && free
-	docker exec postgres_test service postgresql restart &
-	java -cp "/home/mkamras/Eclipses/EE_Luna/workspace/postgres.dbclient/target/postgres.dbclient-0.0.1-SNAPSHOT.jar:/usr/share/java/postgresql-jdbc4-9.2.jar:/home/mkamras/.m2/repository/org/apache/commons/commons-csv/1.4/commons-csv-1.4.jar" postgres.dbclient.DBClient /home/mkamras/psgre_test/phusion/gen/csv/ ; exitCode=$?
+	service postgresql stop
+	nice -n 19 service postgresql start &
+	nice -n -20 java -cp "jars/postgres.dbclient-0.0.1-SNAPSHOT.jar:jars/postgresql-jdbc4-9.2.jar:jars/commons-csv-1.4.jar" postgres.dbclient.DBClient csv/ ; exitCode=$?
 	if [ "$exitCode" -eq "1" ]
 	then
 		echo "Inconsistent state found."
-	else
-		echo "No problems detected."
+		break
 	fi
 	echo "TEST END"
 done
