eca32cff32e8294c341842928375edcf87af2ee7
1forintos <xrensis@gmail.com>
2016-09-29 17:10:51 +0200

    Added files for bugreport. JAVA- added millisecond to log filename endings

---
+++ postgres_bug_report/test_db.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+while true
+do
+	echo "TEST BEGIN"
+	docker exec postgres_9.3.14 free && sync && free
+	docker exec postgres_9.3.14 service postgresql stop
+	docker exec postgres_9.3.14 service postgresql start &
+
+	echo "Running queries..."
+	java -cp "jars/postgres.dbclient-0.0.1-SNAPSHOT.jar:jars/postgresql-jdbc4-9.2.jar:jars/commons-csv-1.4.jar" postgres.dbclient.DBClient gen/csv/ $1 ; exitCode=$?
+	if [ "$exitCode" -eq "1" ]
+	then
+		echo "Records dont match."
+		break
+	fi
+
+	echo "TEST END"
+done
